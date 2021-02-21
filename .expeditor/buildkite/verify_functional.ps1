a779b2a857e0fbffd584b3a2f08aa0265010a69a
Clinton Wolfe <clintoncwolfe@gmail.com>
2021-02-21 14:50:55 -0500

    Split functional tests into 3 jobs
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- .expeditor/buildkite/verify_functional.ps1
+++ .expeditor/buildkite/verify_functional.ps1
@@ -7,7 +7,7 @@ bundle --version
 echo "--- bundle install"
 bundle install --jobs=7 --retry=3 --without tools maintenance deploy
 
-echo "+++ bundle exec rake test:functional"
-bundle exec rake test:functional
+echo "+++ bundle exec rake test:functional slice $SLICE/$SLICES"
+bundle exec rake test:functional SLICES=$SLICES SLICE=$SLICE
 
 exit $LASTEXITCODE
